db71b0d07af119dc6b4172c494e60674c0788e09
LiliannyMarinho <liliannymarinho@gmail.com>
2023-10-29 17:56:35 -0300

    ATL-1

--- linkedList/LinkedList.java
+++ linkedList/LinkedList.java
@@ -5,5 +5,33 @@ public class LinkedList {
     private Node last; //último
     private int size; //tamanho
 
-    
+    public LinkedList(){
+        this.size = 0;
+    }
+    public Node getFirst(){
+        return first;
+    }
+    public void setFirst(Node first){
+        this.first = first;
+    }
+    public Node getLaste(){
+        return last;
+    }
+    public void setLast(Node last){
+        this.last = last;
+    }
+    public int getSize() {
+        return size;
+    }
+    public void setSize(int size) {
+        this.size = size;
+    }
+
+    public void insert(int newdata){
+
+    }
+    public void remove(int newdata){
+
+    }
+
 }

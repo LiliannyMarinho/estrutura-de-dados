aee00aebde298e18f98b32b39bb66bbb06b46c18
LiliannyMarinho <liliannymarinho@gmail.com>
2023-10-29 17:20:05 -0300

    first commit

---
+++ linkedList/LinkedList.java
@@ -0,0 +1,9 @@
+package linkedList;
+
+public class LinkedList {
+    private Node first; //primeiro
+    private Node last; //último
+    private int size; //tamanho
+
+    
+}
---
+++ linkedList/Node.java
@@ -0,0 +1,11 @@
+package linkedList;
+
+public class Node {
+    private int data;
+    private Node prox;
+
+    Node(int data){
+        this.data = data;
+        this.prox = null;
+    }
+}
